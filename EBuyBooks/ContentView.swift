e051014081b90608a0defb34752aad659010f38b
Ramesh <ramesh5mac@gmail.com>
2020-12-03 12:11:43 +0530

    Initial Commit

---
+++ EBuyBooks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EBuyBooks
+//
+//  Created by Ramesh kumar penta on 03/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
